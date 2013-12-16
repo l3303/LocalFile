f8c38fc7cbec2d0cbcb842c4f824c4b9c7e2d1da
l3303 <liuken@163.com>
2013-12-16 17:09:03 +0800

    Intial project

---
+++ LocalFileIO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Com.Ken.File
+{
+    public static class LocalFileIO
+    {
+        public static string ReadFile(string path)
+        {
+            FileStream file = new FileStream(path, FileMode.Open);
+
+            StringBuilder sb = new StringBuilder();
+            using (StreamReader reader = new StreamReader(file))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    sb.Append(line + "\n");
+                }
+            }
+            return sb.ToString();
+        }
+
+        public static void WriteFile(string name, string content, bool rewrite = true)
+        {
+            FileStream file;
+            if (!rewrite)
+            {
+                try
+                {
+                    file = new FileStream(name, FileMode.Append);
+                }
+                catch (FileNotFoundException exp)
+                {
+                    file = new FileStream(name, FileMode.OpenOrCreate);
+                }
+            }
+            else
+            {
+                file = new FileStream(name, FileMode.OpenOrCreate);
+                file.SetLength(0);
+            }
+
+            using (StreamWriter writer = new StreamWriter(file))
+            {
+                StringReader reader = new StringReader(content);
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    writer.WriteLine(line);
+                }
+
+                writer.Close();
+                reader.Close();
+                reader.Dispose();
+            }
+        }
+    }
+}
